561953c354a52767b63d60d9ef965fd70b104749
zverev <alexander.zverev@gmail.com>
2016-01-24 18:51:46 +0300

    move landscape stuff to landscape.js

---
+++ src/landscape.js
@@ -0,0 +1,70 @@
+var fs = require('fs');
+var path = require('path');
+var THREE = require('three');
+var heightmapVShader = fs.readFileSync(path.join(__dirname, 'heightmap.vshader'), 'utf-8');
+var heightmapFShader = fs.readFileSync(path.join(__dirname, 'heightmap.fshader'), 'utf-8');
+
+// texture used to generate "bumpiness"
+var bumpTexture = new THREE.ImageUtils.loadTexture('resources/heightmap.png');
+bumpTexture.wrapS = bumpTexture.wrapT = THREE.RepeatWrapping;
+// magnitude of normal displacement
+var bumpScale = 1.5;
+
+var oceanTexture = new THREE.ImageUtils.loadTexture('resources/dirt-512.jpg');
+oceanTexture.wrapS = oceanTexture.wrapT = THREE.RepeatWrapping;
+
+var sandyTexture = new THREE.ImageUtils.loadTexture('resources/sand-512.jpg');
+sandyTexture.wrapS = sandyTexture.wrapT = THREE.RepeatWrapping;
+
+var grassTexture = new THREE.ImageUtils.loadTexture('resources/grass-512.jpg');
+grassTexture.wrapS = grassTexture.wrapT = THREE.RepeatWrapping;
+
+var rockyTexture = new THREE.ImageUtils.loadTexture('resources/snow-rocks-512.jpg');
+rockyTexture.wrapS = rockyTexture.wrapT = THREE.RepeatWrapping;
+
+var snowyTexture = new THREE.ImageUtils.loadTexture('resources/snow-512.jpg');
+snowyTexture.wrapS = snowyTexture.wrapT = THREE.RepeatWrapping;
+
+var customUniforms = {
+    bumpTexture: {
+        type: "t",
+        value: bumpTexture
+    },
+    bumpScale: {
+        type: "f",
+        value: bumpScale
+    },
+    oceanTexture: {
+        type: "t",
+        value: oceanTexture
+    },
+    sandyTexture: {
+        type: "t",
+        value: sandyTexture
+    },
+    grassTexture: {
+        type: "t",
+        value: grassTexture
+    },
+    rockyTexture: {
+        type: "t",
+        value: rockyTexture
+    },
+    snowyTexture: {
+        type: "t",
+        value: snowyTexture
+    },
+};
+
+// create custom material from the shader code above
+//   that is within specially labelled script tags
+var customMaterial = new THREE.ShaderMaterial({
+    uniforms: customUniforms,
+    vertexShader: heightmapVShader,
+    fragmentShader: heightmapFShader
+        // side: THREE.DoubleSide
+});
+
+var planeGeo = new THREE.PlaneGeometry(32, 32, 400, 400);
+
+module.exports = new THREE.Mesh(planeGeo, customMaterial);
