2160e37c028c47788638411ce436f167d8221be9
zverev <alexander.zverev@gmail.com>
2016-01-30 14:34:25 +0300

    fix border vertices

--- src/landscape.js
+++ src/landscape.js
@@ -68,7 +68,7 @@ var customMaterial = new THREE.ShaderMaterial({
 var textureMaterial = new THREE.MeshBasicMaterial( { map: bumpTexture } );
 
 var planeSize = 32;
-var planeDetails = 64;
+var planeDetails = 32;
 var maxHeight = 2;
 
 var planeGeo = new THREE.PlaneGeometry(planeSize, planeSize, planeDetails, planeDetails);
@@ -80,20 +80,10 @@ img.onload = function() {
     canvas.height = img.height;
     var ctx = canvas.getContext('2d');
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-    window.cv = canvas;
-
-    var rawData = ctx.getImageData(0, 0, planeDetails * 2, planeDetails * 2).data;
-    var pixData = [];
-    for (var i = 0, c=0; i < rawData.length; i+=4, c++) {
-        pixData[c] = rawData[i];
-    }
-
-    console.log(pixData);
-    console.log(pixData.length);
 
     planeGeo.dynamic = true;
 
-    var s = canvas.width / planeDetails;
+    var s = canvas.width / (planeDetails + 1);
 
     for (var i = 0; i < planeDetails + 1; i++) {
         for (var j = 0; j < planeDetails + 1; j++) {
@@ -105,20 +95,9 @@ img.onload = function() {
         return (ctx.getImageData(s * j, s * i, 1, 1).data[0] / 255) * maxHeight;
     }
 
-    function getAverangePixelValue(ctx, x1, y1, x2, y2) {
-        var v = 0;
-
-        for (var i = y1; i < y2; i++) {
-            for (var j = x1; j < x2; j++) {
-                v += pixData[i * canvas.height + j];
-            }
-        }
-
-        return v / (Math.abs(x2 - x1) * Math.abs(y2 - y1));
-    }
-
     planeGeo.verticesNeedUpdate = true;
 };
 img.src = 'resources/heightmap.png';
-module.exports = new THREE.Mesh(planeGeo, customMaterial);
+//module.exports = new THREE.Mesh(planeGeo, customMaterial);
 //module.exports = new THREE.Mesh(planeGeo, textureMaterial);
+module.exports = new THREE.Mesh(planeGeo, new THREE.MeshBasicMaterial({ color: 0xdddddd, wireframe: true }))
