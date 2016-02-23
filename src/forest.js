386a8a4bae0b9ef62320283acb76099498d6b726
zverev <alexander.zverev@gmail.com>
2016-02-23 19:21:14 +0300

    utils: getCoordinatesFromMap() function

--- src/forest.js
+++ src/forest.js
@@ -1,23 +1,17 @@
 var THREE = require('three');
 var createFir = require('./fir.js');
 var config = require('./config.js');
+var utils = require('./utils.js');
 
 module.exports = function() {
     return new Promise(function(resolve, reject) {
-        loadImage(config.forestMap).then(function(image) {
+        utils.getCoordinatesFromMap(config.forestMap).then(function(coords) {
             var forest = new THREE.Object3D();
-            var canvas = document.createElement('canvas');
-            window.canvas = canvas;
-            canvas.width = image.width;
-            canvas.height = image.height;
-            var ctx = canvas.getContext('2d');
-            ctx.drawImage(image, 0, 0, image.width, image.height);
-            var treePixels = getTreePixels(canvas, ctx);
-            for (var i = 0; i < treePixels.length; i++) {
-                var p = treePixels[i];
+
+            for (var i = 0; i < coords.length; i++) {
+                var c = coords[i];
                 var fir = createFir(config.firHeight);
-                var l = config.landscapePlaneSize;
-                fir.position.set(-p.y / canvas.width * l + l / 2, -p.x / canvas.height * l + l / 2, 0);
+                fir.position.set(c.x, c.y, c.z);
                 forest.add(fir);
             }
 
@@ -49,40 +43,4 @@ function createForestPlane(map) {
     //mesh.rotateY(Math.PI/3)
     mesh.position.setZ(1.5);
     return mesh;
-}
-
-function getRandomPixel(canvas, ctx) {
-    do {
-        var x = Math.random() * canvas.width;
-        var y = Math.random() * canvas.height;
-        var p = ctx.getImageData(x, y, 1, 1);
-    } while (p.data[0] < 127)
-    return {
-        x: x,
-        y: y
-    }
-}
-
-function getTreePixels(canvas, ctx) {
-    var pixels = [];
-    var data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
-    for (var i = 0; i < data.length; i += 4) {
-        if (data[i] > 127) {
-            pixels.push({
-                x: i / 4 % canvas.width,
-                y: Math.floor(i / 4 / canvas.height)
-            })
-        }
-    }
-    return pixels;
-}
-
-function loadImage(src) {
-    return new Promise(function(resolve, reject) {
-        var img = new Image();
-        img.onload = function() {
-            resolve(img);
-        };
-        img.src = src;
-    });
 }
--- src/utils.js
+++ src/utils.js
@@ -1,41 +1,81 @@
 var THREE = require('three');
 var ColladaLoader = require('./ColladaLoader');
+var config = require('./config.js');
+
+function loadScene(url) {
+    return new Promise(function(resolve, reject) {
+        // instantiate a loader
+        var loader = new ColladaLoader();
+
+        loader.load(
+            // resource URL
+            url,
+            // Function when resource is loaded
+            function(collada) {
+                resolve(collada.scene);
+            },
+            // Function called when download progresses
+            function(xhr) {
+                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+            }
+        );
+    });
+}
+
+function loadImage(url) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            var img = new Image();
+            img.onload = function() {
+                resolve(img);
+            };
+            img.onerror = function() {
+                reject();
+            }
+            img.src = url;
+        }, 0);
+    })
+}
+
+function getCoordinatesFromMap(imgPath) {
+    return new Promise(function (resolve, reject) {
+        loadImage(config.forestMap).then(function(image) {
+            var canvas = document.createElement('canvas');
+            window.canvas = canvas;
+            canvas.width = image.width;
+            canvas.height = image.height;
+            var ctx = canvas.getContext('2d');
+            ctx.drawImage(image, 0, 0, image.width, image.height);
+            var pixels = getMapPixels(canvas, ctx);
+
+            var coords = [];
+            for (var i = 0; i < pixels.length; i++) {
+                var p = pixels[i];
+                var l = config.landscapePlaneSize;
+                coords.push(new THREE.Vector3(-p.y / canvas.width * l + l / 2, -p.x / canvas.height * l + l / 2, 0));
+            }
+
+            resolve(coords);
+        });
+    });
+}
+
+function getMapPixels(canvas, ctx) {
+    var pixels = [];
+    var data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+    for (var i = 0; i < data.length; i += 4) {
+        if (data[i] > 127) {
+            pixels.push({
+                x: i / 4 % canvas.width,
+                y: Math.floor(i / 4 / canvas.height)
+            })
+        }
+    }
+    return pixels;
+}
 
 module.exports = {
-
-    loadScene: function(url) {
-        return new Promise(function(resolve, reject) {
-            // instantiate a loader
-            var loader = new ColladaLoader();
-
-            loader.load(
-                // resource URL
-                url,
-                // Function when resource is loaded
-                function(collada) {
-                    resolve(collada.scene);
-                },
-                // Function called when download progresses
-                function(xhr) {
-                    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-                }
-            );
-        });
-    },
-
-    loadImage: function(url) {
-        return new Promise(function(resolve, reject) {
-            setTimeout(function() {
-                var img = new Image();
-                img.onload = function() {
-                    resolve(img);
-                };
-                img.onerror = function() {
-                    reject();
-                }
-                img.src = url;
-            }, 0);
-        })
-    }
-
-};
+    loadScene: loadScene,
+    loadImage: loadImage,
+    getCoordinatesFromMap: getCoordinatesFromMap
+}
